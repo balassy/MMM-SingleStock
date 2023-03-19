34c26781f4e57b26f8d1df2c245362b505186b0e
Tara Dadah <tarad@spotify.com>
2023-03-19 19:07:26 -0400

    add Tiingo API

--- MMM-SingleStock.js
+++ MMM-SingleStock.js
@@ -14,10 +14,12 @@ Module.register('MMM-SingleStock', {
     changeType: '',
     colorized: false,
     minimal: false,
-    label: 'symbol' // 'symbol' | 'companyName' | 'none'
+    label: 'symbol', // 'symbol' | 'companyName' | 'none'
+    api: 'iexcloud' // 'iexcloud' | 'tiingo'
   },
 
   requiresVersion: '2.1.0',
+  url: '',
 
   getTranslations() {
     return {
@@ -71,14 +73,11 @@ Module.register('MMM-SingleStock', {
           ? 'dimmed xsmall'
           : 'dimmed small';
 
-        if (this.config.colorized)
-        {
-          if (this.viewModel.change > 0)
-          {
+        if (this.config.colorized) {
+          if (this.viewModel.change > 0) {
             changeEl.style = 'color: #a3ea80';
           }
-          if (this.viewModel.change < 0)
-          {
+          if (this.viewModel.change < 0) {
             changeEl.style = 'color: #FF8E99';
           }
         }
@@ -98,10 +97,10 @@ Module.register('MMM-SingleStock', {
   _getData(onCompleteCallback) {
     const self = this;
 
-    const url = `https://cloud.iexapis.com/v1/stock/${this.config.stockSymbol}/quote?token=${this.config.apiToken}`;
+    self._SetUrl(this.config.api);
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open('GET', self.url, true);
     xhr.onreadystatechange = function onReadyStateChange() {
       if (this.readyState === 4) {
         if (this.status === 200) {
@@ -117,27 +116,29 @@ Module.register('MMM-SingleStock', {
   },
 
   _processResponse(responseBody) {
-    const response = JSON.parse(responseBody);
+    const response = this._setResponse(responseBody);
+
+    Log.info(response);
 
     this.viewModel = {
-      price: response.latestPrice
+      price: this._setPrice(response)
     };
 
     switch (this.config.changeType) {
       case 'percent':
-        this.viewModel.change = (response.changePercent * 100).toFixed(2);
+        this.viewModel.change = this._setPercentChange(response);
         break;
       default:
-        this.viewModel.change = response.change > 0 ? `+${response.change}` : `${response.change}`;
+        this.viewModel.change = this._setNumericChange(response);
         break;
     }
 
     switch (this.config.label) {
       case 'symbol':
-        this.viewModel.label = response.symbol;
+        this.viewModel.label = response.ticker;
         break;
       case 'companyName':
-        this.viewModel.label = response.companyName;
+        this.viewModel.label = response.ticker;
         break;
       case 'none':
         this.viewModel.label = '';
@@ -152,5 +153,196 @@ Module.register('MMM-SingleStock', {
     }
 
     this.hasData = true;
+  },
+
+  _setResponse(responseBody) {
+    let response = '';
+    switch (this.config.api) {
+      case 'iexcloud': {
+        response = JSON.parse(responseBody);
+        break;
+      }
+      case 'tiingo': {
+        const parsed = JSON.parse(responseBody);
+        // eslint-disable-next-line prefer-destructuring
+        response = parsed[0];
+        break;
+      }
+      default:
+        break;
+    }
+
+    return response;
+  },
+
+  _setPrice(response) {
+    let value = '';
+    switch (this.config.api) {
+      case 'iexcloud':
+        value = response.latestPrice;
+        break;
+      case 'tiingo':
+        value = response.last;
+        break;
+      default:
+        break;
+    }
+
+    return value;
+  },
+
+  _setNumericChange(response) {
+    let value = '';
+    switch (this.config.api) {
+      case 'iexcloud':
+        value = response.change > 0 ? `+${response.change}` : `${response.change}`;
+        break;
+      case 'tiingo':
+        value = (response.prevClose - response.last).toFixed(2);
+        break;
+      default:
+        break;
+    }
+
+    return value;
+  },
+
+  _setPercentChange(response) {
+    let value = '';
+    switch (this.config.api) {
+      case 'iexcloud':
+        value = (response.changePercent * 100).toFixed(2);
+        break;
+      case 'tiingo':
+        value = (
+          ((response.prevClose - response.last) / response.prevClose)
+          * 100
+        ).toFixed(2);
+        break;
+      default:
+        break;
+    }
+
+    return value;
+  },
+
+  _SetUrl(api) {
+    switch (api) {
+      case 'iexcloud':
+        this.url = `https://cloud.iexapis.com/v1/stock/${this.config.stockSymbol}/quote?token=${this.config.apiToken}`;
+        break;
+      case 'tiingo':
+        if (this.url === '') {
+          this.url = this._getTiingoUrl();
+        }
+        break;
+      default:
+        Log.error(this.name, `Invalid api config: ${api}`);
+        break;
+    }
+  },
+
+  _getTiingoUrl() {
+    const expectedResponseHeaders = [
+      'server',
+      'date',
+      'content-type',
+      'content-length',
+      'vary',
+      'x-frame-options'
+    ];
+    const requestHeaders = [
+      { name: 'Content-Type', value: 'application/json' },
+      { name: 'Authorization', value: `Token ${this.config.apiToken}` }
+    ];
+
+    const url = this._getCorsUrl(
+      `https://api.tiingo.com/iex/?tickers=${this.config.stockSymbol}`,
+      requestHeaders,
+      expectedResponseHeaders
+    );
+
+    return url;
+  },
+
+  /**
+   * Gets a URL that will be used when calling the CORS-method on the server.
+   *
+   * @param {string} url the url to fetch from
+   * @param {Array.<{name: string, value:string}>} requestHeaders the HTTP headers to send
+   * @param {Array.<string>} expectedResponseHeaders the expected HTTP headers to recieve
+   * @returns {string} to be used as URL when calling CORS-method on server.
+   */
+  _getCorsUrl(url, requestHeaders, expectedResponseHeaders) {
+    if (!url || url.length < 1) {
+      throw new Error(`Invalid URL: ${url}`);
+    } else {
+      // eslint-disable-next-line no-restricted-globals
+      let corsUrl = `${location.protocol}//${location.host}/cors?`;
+
+      const requestHeaderString = this._getRequestHeaderString(requestHeaders);
+      if (requestHeaderString) corsUrl = `${corsUrl}sendheaders=${requestHeaderString}`;
+
+      // eslint-disable-next-line max-len
+      const expectedResponseHeadersString = this._getExpectedResponseHeadersString(expectedResponseHeaders);
+      if (requestHeaderString && expectedResponseHeadersString) {
+        corsUrl = `${corsUrl}&expectedheaders=${expectedResponseHeadersString}`;
+      } else if (expectedResponseHeadersString) {
+        corsUrl = `${corsUrl}expectedheaders=${expectedResponseHeadersString}`;
+      }
+
+      if (requestHeaderString || expectedResponseHeadersString) {
+        return `${corsUrl}&url=${url}`;
+      }
+      return `${corsUrl}url=${url}`;
+    }
+  },
+
+  /**
+   * Gets the part of the CORS URL that represents the HTTP headers to send.
+   *
+   * @param {Array.<{name: string, value:string}>} requestHeaders the HTTP headers to send
+   * @returns {string} to be used as request-headers component in CORS URL.
+   */
+  _getRequestHeaderString(requestHeaders) {
+    let requestHeaderString = '';
+    if (requestHeaders) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const header of requestHeaders) {
+        if (requestHeaderString.length === 0) {
+          requestHeaderString = `${header.name}:${encodeURIComponent(
+            header.value
+          )}`;
+        } else {
+          requestHeaderString = `${requestHeaderString},${
+            header.name
+          }:${encodeURIComponent(header.value)}`;
+        }
+      }
+      return requestHeaderString;
+    }
+    return undefined;
+  },
+
+  /**
+   * Gets the part of the CORS URL that represents the expected HTTP headers to recieve.
+   *
+   * @param {Array.<string>} expectedResponseHeaders the expected HTTP headers to recieve
+   * @returns {string} to be used as the expected HTTP-headers component in CORS URL.
+   */
+  _getExpectedResponseHeadersString(expectedResponseHeaders) {
+    let expectedResponseHeadersString = '';
+    if (expectedResponseHeaders) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const header of expectedResponseHeaders) {
+        if (expectedResponseHeadersString.length === 0) {
+          expectedResponseHeadersString = `${header}`;
+        } else {
+          expectedResponseHeadersString = `${expectedResponseHeadersString},${header}`;
+        }
+      }
+      return expectedResponseHeaders;
+    }
+    return undefined;
   }
 });
